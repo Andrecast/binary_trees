7bcc1629009959dca3c3787b30fb2c9718eaaf8d
vanemcb <vanem2201@gmail.com>
2021-07-21 11:14:31 -0500

    Function modified

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,35 +7,51 @@
  **/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int depth;
+
 	if (!tree)
 		return (0);
 
-	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return (1);
-	return (0);
+	depth = binary_tree_depth_2(tree);
+
+	return (is_perfect(tree, depth, 0));
+
 }
 
 /**
- * binary_tree_height - function that goes through a binary
- * tree using post-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * Return: size
+ * binary_tree_depth_2 - Calculate the depth
+ * @tree: pointer to the root node of the tree
+ * Return: depth
  **/
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_depth_2(const binary_tree_t *tree)
 {
-	size_t node_left = 0, node_right = 0;
+	int depth = 0;
 
-	if (!tree  || (!tree->left && !tree->right))
-		return (0);
-
-
-	node_left = binary_tree_height(tree->left);
-	node_right = binary_tree_height(tree->right);
-
-	if (node_left > node_right)
+	while (tree)
 	{
-		return (node_left + 1);
+		depth++;
+		tree = tree->left;
 	}
 
-	return (node_right + 1);
+	return (depth);
+}
+
+/**
+ * is_perfect - function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree
+ * @depth: binary tree deepth
+ * @level: level
+ * Return: 1 if binary tree is perfect
+ */
+int is_perfect(const binary_tree_t *tree, int depth, int level)
+{
+	if (!tree->left && !tree->right)
+		if (depth == level + 1)
+			return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
+	return (is_perfect(tree->left, depth, level + 1) && is_perfect(
+		tree->right, depth, level + 1));
 }

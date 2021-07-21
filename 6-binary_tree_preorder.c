d700ffdd2ecbdf7a884f99daea43e537c1aef5ba
Andrecast <acastrillonpuerta@gmail.com>
2021-07-21 18:30:41 +0700

    task 6 fixed

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,15 +9,10 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree && !func)
-		return;
-
-	func(tree->n);
-
-	if (tree->left)
+	if (tree && func)
+	{
+		func(tree->n);
 		binary_tree_preorder(tree->left, func);
-
-	if (tree->right)
 		binary_tree_preorder(tree->right, func);
-
+	}
 }
